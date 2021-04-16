6c95fe6e8d3fe61c446d955becce5f92043f7da9
Manuel Lopez <mlopez@mdsdigital.com>
2021-04-16 11:15:11 -0500

    Funciones faltantes para funcion sanitize_title

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -787,7 +787,7 @@ define('LIST_COUNTRYS_CODES',array_map(function($country){
 
 
 
-function sanitize_title( $title, $fallback_title = '', $context = 'save' ) {
+function sanitize_title( $title, $lower = true, $fallback_title = '', $context = 'save' ) {
     $raw_title = $title;
  
     if ( 'save' === $context ) {
@@ -798,7 +798,7 @@ function sanitize_title( $title, $fallback_title = '', $context = 'save' ) {
         $title = $fallback_title;
     }
  
-    return $title;
+    return $lower ? strtolower($title) : $title;
 }
 function remove_accents( $string ) {
     if ( ! preg_match( '/[\x80-\xff]/', $string ) ) {
@@ -1205,4 +1205,59 @@ function get_locale() {
      * @param string $locale The locale ID.
      */
     return $locale;
+}
+function seems_utf8( $str ) {
+    mbstring_binary_safe_encoding();
+    $length = strlen( $str );
+    reset_mbstring_encoding();
+    for ( $i = 0; $i < $length; $i++ ) {
+        $c = ord( $str[ $i ] );
+        if ( $c < 0x80 ) {
+            $n = 0; // 0bbbbbbb
+        } elseif ( ( $c & 0xE0 ) == 0xC0 ) {
+            $n = 1; // 110bbbbb
+        } elseif ( ( $c & 0xF0 ) == 0xE0 ) {
+            $n = 2; // 1110bbbb
+        } elseif ( ( $c & 0xF8 ) == 0xF0 ) {
+            $n = 3; // 11110bbb
+        } elseif ( ( $c & 0xFC ) == 0xF8 ) {
+            $n = 4; // 111110bb
+        } elseif ( ( $c & 0xFE ) == 0xFC ) {
+            $n = 5; // 1111110b
+        } else {
+            return false; // Does not match any model.
+        }
+        for ( $j = 0; $j < $n; $j++ ) { // n bytes matching 10bbbbbb follow ?
+            if ( ( ++$i == $length ) || ( ( ord( $str[ $i ] ) & 0xC0 ) != 0x80 ) ) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+function mbstring_binary_safe_encoding( $reset = false ) {
+    static $encodings  = array();
+    static $overloaded = null;
+ 
+    if ( is_null( $overloaded ) ) {
+        $overloaded = function_exists( 'mb_internal_encoding' ) && ( ini_get( 'mbstring.func_overload' ) & 2 );
+    }
+ 
+    if ( false === $overloaded ) {
+        return;
+    }
+ 
+    if ( ! $reset ) {
+        $encoding = mb_internal_encoding();
+        array_push( $encodings, $encoding );
+        mb_internal_encoding( 'ISO-8859-1' );
+    }
+ 
+    if ( $reset && $encodings ) {
+        $encoding = array_pop( $encodings );
+        mb_internal_encoding( $encoding );
+    }
+}
+function reset_mbstring_encoding() {
+    mbstring_binary_safe_encoding( true );
 }
