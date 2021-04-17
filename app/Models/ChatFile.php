06413a786a207bb0374322e6ff9daf2ec92e2115
Manuel <mlopez@mdsdigital.com>
2021-04-17 14:28:15 -0500

    Correccion import de clase Storage en modelo ChatFile

--- app/Models/ChatFile.php
+++ app/Models/ChatFile.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Carbon\Carbon;
+use App\Exceptions\WanderException;
+
 
 class ChatFile extends Model
 {
